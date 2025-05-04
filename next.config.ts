ecd1550c125888d616cdd1125e4011a0a6302fad
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2025-05-04 22:29:14 +0330

    Implemented PWA

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,36 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 import type { NextConfig } from "next";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  dynamicStartUrl: true,
+  dynamicStartUrlRedirect: "/login",
+  reloadOnOnline: true,
+  extendDefaultRuntimeCaching: true,
+  workboxOptions: {
+    cacheId: "v-1.0.0",
+    skipWaiting: true,
+    clientsClaim: true,
+    cleanupOutdatedCaches: true,
+    disableDevLogs: true,
+    runtimeCaching: [
+      {
+        urlPattern: new RegExp(
+          `${process.env.NEXT_PUBLIC_HTTP_URL}/v1/users/login`
+        ),
+        handler: "NetworkOnly",
+      },
+      {
+        urlPattern: new RegExp(
+          `${process.env.NEXT_PUBLIC_HTTP_URL}/v1/users/login/verification-login`
+        ),
+        handler: "NetworkOnly",
+      },
+    ],
+    exclude: [/\.mp4$/],
+  },
+});
 
 const nextConfig: NextConfig = {
   images: {
@@ -9,6 +41,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
