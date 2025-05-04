a6eb0f1b399925b7c6537e7a5526df89c45b9d2d
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2025-05-04 22:44:40 +0330

    Refactor metadata structure for improved clarity and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,72 @@ const vazirmatn = Vazirmatn({
   subsets: ["latin"],
 });
 
+const APP_NAME = "HelpHub";
+const APP_DESCRIPTION =
+  "A modern support chat application for assisting users.";
+
 export const metadata: Metadata = {
-  title: "HelpHub",
-  description: "A modern support chat application for assisting users.",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_NAME,
+    template: "%s - HelpHub App",
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  metadataBase: new URL("https://yourdomain.com"),
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_NAME,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/images/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/images/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/images/android-chrome-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+      url: "/images/android-chrome-512x512.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/images/apple-touch-icon.png",
+    },
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://amestr.ir",
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+    images: [
+      {
+        url: "/images/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "HelpHub App",
+      },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
